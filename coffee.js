27f0198064374fcb31a03af2215be16c17f20aac
jojayeon <jojayeon6152@gmail.com>
2024-06-03 15:41:51 +0900

    생성자 함수 기본형

---
+++ coffee.js
@@ -0,0 +1,12 @@
+
+class makeCoffee {
+  constructor(wondoo, water, sugar, milk) {
+    this.wondoo = wondoo;
+    this.water = water;
+    this.sugar = sugar;
+    this.milk =  milk;
+  }
+}
+
+let test = new makeCoffee("대전", 300, true, true);
+console.log(test);
