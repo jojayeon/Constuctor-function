262ff0223da99f9dad5ff08b60f1ed51bb5451a8
jojayeon <jojayeon6152@gmail.com>
2024-06-03 15:37:03 +0900

    old로 함수와 규격비교

--- old.js
+++ old.js
@@ -4,3 +4,18 @@ function old(a,b){
 }
 let test = new old(1,2);
 console.log(test);
+
+
+//함수
+
+//규격
+
+
+class Newold {
+  constructor(a,b){
+    this.frist = a;
+    this.second = b;
+  }
+}
+let test2 = new old(1,2);
+console.log(test2);
