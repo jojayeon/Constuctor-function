b1e3384f283fd03d8ac5365888f15a4039a9ae00
jojayeon <jojayeon6152@gmail.com>
2024-06-03 12:05:11 +0900

    함수 능력  바꿈

--- work-3.js
+++ work-3.js
@@ -1,6 +1,9 @@
-function WorkThree(a,b) {
-  let local ={};
-  local[a] = a;
-  local[b] = b;
+function WorkThree(index,name) {
+  let local = {
+    number : index+1,
+    name :name 
+  };
   return local;
 };
+let test = WorkThree(0,"조자연");
+console.log(test);
